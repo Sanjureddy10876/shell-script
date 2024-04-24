2909179545135827068c6eb76e903e0777f9d3b0
Sanju-Reddy <reddysanju9898@gmail.com>
2024-04-24 19:03:46 +0530

    some-message

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Sai
+PERSON2=Santhosh
+
+
+echo "$PERSON1:: hello $PERSON2 how are you"
+echo "$PERSON2:: iam good what about you"
+echo "$PERSON1:: iam fine and how is your work"
+echo "$PERSON2:: doing good"
