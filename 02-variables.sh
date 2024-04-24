1ccc376ef43f4e5f8016742da7e1ce16b43738eb
Sanju-Reddy <reddysanju9898@gmail.com>
2024-04-24 18:56:06 +0530

    some-message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is again sample script
+#!/bin/bash
+
+echo "Sai : hello santhosh how are you"
+echo "Santhosh : iam good what about you"
+echo "Sai : iam fine and how is your work"
+echo "Santhosh : doing good"
