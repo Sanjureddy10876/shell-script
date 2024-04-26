301ec4a3fc65f2be53f9cb36f96300d0b36c7fd9
Sanju-Reddy <reddysanju9898@gmail.com>
2024-04-26 18:16:15 +0530

    some-message

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(basename "$0" .sh)
-LOGFILE=/temp/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -17,15 +17,14 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
 then 
-    echo "Please run this script with not access."
-    exit 1 #manuvally exit if error comes.
+    echo "Please run this script with root access."
+    exit 1
 else
-    echo "Your are super user."
+    echo "You are the super user."
 fi
 
 dnf install mysql -y &>>"$LOGFILE"
 VALIDATE $? "Installing MySQL"
 
 dnf install git -y &>>"$LOGFILE"
-VALIDATE $? "Installing MySQL"
-
+VALIDATE $? "Installing Git"
