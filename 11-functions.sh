f600b73081f8975e67a988eaec25028a033ef457
Sanju-Reddy <reddysanju9898@gmail.com>
2024-04-26 17:55:59 +0530

    some-message

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,18 +5,14 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/temp/$SCRIPT_NAME-$TIMESTAMP.log
 
-
 VALIDATE(){
-
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
-        exit 1
-
+        echo "$2....FAILED"
+        exit 1 
     else 
         echo "$2....SUCCESS"
     fi
-
 }
 
 if [ $USERID -ne 0 ]
@@ -28,10 +24,8 @@ else
 fi
 
 dnf install mysql -y &>>$LOGFILE
-
 VALIDATE $? "Installing MySQL"
 
 dnf install git -y &>>$LOGFILE
-
 VALIDATE $? "Installing MySQL"
 
