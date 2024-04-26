90fccdebecee4b29d7532b6c4a163072cea4d7a9
Sanju-Reddy <reddysanju9898@gmail.com>
2024-04-26 12:32:26 +0530

    some-message

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "Please run this script with not access."
+    exit 1 #manuvally exit if error comes.
 else
     echo "Your are super user."
 fi
