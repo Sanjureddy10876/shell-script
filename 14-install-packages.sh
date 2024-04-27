e0ad81e7c264a61e74d609198eeffbbbbbb74e7c
Sanju-Reddy <reddysanju9898@gmail.com>
2024-04-27 10:55:42 +0530

    some-message

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -16,10 +16,10 @@ N='\033[0m'
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2....$R FAILED $N"
+        echo -e "$2....$R FAILED $N"
         exit 1 
     else 
-        echo "$2....$G SUCCESS $N"
+        echo -e "$2....$G SUCCESS $N"
     fi
 }
 
