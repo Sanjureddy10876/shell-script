a7e3ee16da552ac0beff780f5c7dfb29d10c79af
Sanju-Reddy <reddysanju9898@gmail.com>
2024-04-27 10:44:33 +0530

    some-message

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,6 +4,14 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(basename "$0" .sh)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+P="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[33m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -29,7 +37,7 @@ do
     dnf list installed $i &>>$LOGFILE
     if [ $? -eq 0 ]
     then
-        echo "Package $i is already installed."
+        echo "Package $i is already installed...$Y SKIPPING $N"
     else 
         echo "Package $i is not installed...Need to install"
     fi
